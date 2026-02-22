a53016fe6bd3ec8c41d6ade2f2d902d6a8afc981
BeLfayza <abelfayza37@gmail.com>
2026-02-22 23:49:42 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Modul buat ningkatin Saturasi Layar 2x
+Flash pake **KernelSU Manager** atau **Magisk Manager**.
---
+++ service.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+
+sleep 30 # 30s delay
+
+service call SurfaceFlinger 1022 f 2.0 # android debugging buat ngotak ngatik saturation ygy
